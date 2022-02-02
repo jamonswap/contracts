7fb67f0072ec9b15fa67609c11b0b82fc4feda92
jamonswap <95988459+jamonswap@users.noreply.github.com>
2022-02-02 22:44:35 +0100

    Add view functions and fix issues

--- tokens/JamonShare.sol
+++ tokens/JamonShare.sol
@@ -1,14 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
+pragma solidity =0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract JamonShare is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
-    constructor() ERC20("JamonShare", "JS") ERC20Permit("JamonShare") {
+    
+    using EnumerableSet for EnumerableSet.AddressSet;  
+    using SafeMath for uint256;
+
+    EnumerableSet.AddressSet private taxExent;
+
+    constructor() ERC20("JamonShare", "J-SHARE") ERC20Permit("JamonShare") {
         _mint(msg.sender, 2000000 * 10 ** decimals());
     }
 
@@ -16,7 +24,25 @@ contract JamonShare is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
         _mint(to, amount);
     }
 
+    function setExent(address exent, bool set) external onlyOwner {
+        require(exent != address(0x0), "Invalid address");
+        if(set){
+            taxExent.add(exent);
+        } else {
+            taxExent.remove(exent);
+        }
+    }
+
     // The following functions are overrides required by Solidity.
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        if(!taxExent.contains(_msgSender())) {
+            uint256 toBurn = amount.mul(50).div(10000); // 0.5% tax burn
+            _burn(_msgSender(), toBurn);
+            amount -= toBurn;
+        }
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    } 
 
     function _afterTokenTransfer(address from, address to, uint256 amount)
         internal
